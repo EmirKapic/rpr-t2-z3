4540caf9148776883438b44f606fbc729013d4c3
EmirKapic <ekapic1@etf.unsa.ba>
2022-10-24 19:35:32 +0200

    t

--- src/main/java/ba/unsa/etf/rpr/Broj.java
+++ src/main/java/ba/unsa/etf/rpr/Broj.java
@@ -1,2 +1,10 @@
-package ba.unsa.etf.rpr;public class Broj {
+package ba.unsa.etf.rpr;
+
+public class Broj {
+        static boolean isNumber(String s){
+        for (int i = 0; i < s.length(); ++i)
+            if (!Character.isDigit(s.charAt(i)))
+                return false;
+        return true;
+    }
 }
--- src/main/java/ba/unsa/etf/rpr/Zadatak3.java
+++ src/main/java/ba/unsa/etf/rpr/Zadatak3.java
@@ -1,7 +1,38 @@
 package ba.unsa.etf.rpr;
 
-public class Main {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+import java.util.List;
+import static ba.unsa.etf.rpr.Broj.isNumber;
+import static java.lang.System.exit;
+
+public class Zadatak3 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        List<Integer> L = new ArrayList<>();
+        System.out.println("Unesite brojeve:");
+        while(true){
+            String unos = sc.nextLine();
+            if (unos.equals("stop"))
+                exit(0);
+            if (isNumber(unos)){
+                L.add(Integer.valueOf(unos));
+                System.out.println("Minimalna i maksimalna vrijednost su : " + Collections.min(L) + " " + Collections.max(L));
+                Double sum = 0D;
+                for (Integer x : L)sum += x;
+                Double srednjaVrijednost = sum/L.size();
+                System.out.println("Srednja vrijednost je: " + srednjaVrijednost);
+                sum = 0D;
+                for (Integer x : L)sum += Math.pow(x - srednjaVrijednost,2);
+                sum /= L.size() - 1;
+                System.out.println("Standardna devijacija je: " + Math.sqrt(sum));
+            }
+            else{
+                System.out.println("Neispravan unos, probajte ponovo:");
+            }
+
+        }
     }
 }
